331e891f0590753a630a3b7ab9fd051665d91c70
Paulo Henrique Rodrigues Pinheiro <paulohrpinheiro@gmail.com>
2018-09-11 17:54:44 -0300

    Added GO tests

---
+++ 000_novice/go/problems_test.go
@@ -0,0 +1,47 @@
+package main
+
+import "testing"
+
+func TestNegue(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestDigaOla(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestListaNumerosPares(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestListaMultiplos(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestSoma(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestSubtracao(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestMultiplicacao(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestDivisao(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestOperacao(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestMaior(t *testing.T) {
+	t.Fatal("not implemented")
+}
+
+func TestIntersecao(t *testing.T) {
+	t.Fatal("not implemented")
+}
